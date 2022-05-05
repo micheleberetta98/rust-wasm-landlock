1ca4217dbadd35a24fba538a80b2d9ad18cb3b16
micheleberetta98 <beretta.m98@gmail.com>
2022-05-05 16:46:36 +0200

    Minor changes to PathAccess struct

--- src/path_access.rs
+++ src/path_access.rs
@@ -1,18 +1,18 @@
 // A struct able to iterate landlock's PathBeneath rules
 pub struct PathAccess {
   paths: String,
-  access: Vec<u8>,
+  access: u64,
 }
 
 impl PathAccess {
-  fn new(paths: &str, access: Vec<u8>) -> Self {
+  pub fn new(paths: &str, access: u64) -> Self {
     Self {
       paths: paths.to_owned(),
       access: access,
     }
   }
 
-  fn iter(&self) -> impl Iterator<Item = &str> {
+  pub fn iter(&self) -> impl Iterator<Item = &str> {
     self.paths.split(':').skip_while(|s| s.is_empty())
   }
 }
