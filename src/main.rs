e4f49cc38bc7cc4629f1f85ef7c40af43c1b4115
micheleberetta98 <beretta.m98@gmail.com>
2022-05-12 11:20:41 +0200

    Add simple invocation of hardocded typed function

--- src/main.rs
+++ src/main.rs
@@ -30,8 +30,12 @@ fn main() -> Result<()> {
     .add_rules(get_all_allows(&args))?
     .enforce()?;
 
-  // Run the default export function in module
-  module.run()
+  if let Some(func_name) = args.invoke {
+    println!("{:?}", module.invoke::<(), i32>(&func_name, ())?);
+    Ok(())
+  } else {
+    module.run()
+  }
 }
 
 fn get_all_allows(args: &Args) -> Vec<PathAccess> {
