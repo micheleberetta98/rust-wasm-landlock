b9aefe5324451aec72f5064f05fe2fb3e43f92e3
micheleberetta98 <beretta.m98@gmail.com>
2022-08-01 17:32:42 +0200

    Add some graphs

--- perf-helper.py
+++ perf-helper.py
@@ -2,6 +2,7 @@
 
 import os
 import pandas as pd
+import matplotlib.pyplot as plt
 
 exe = "./target/release/rust-wasm-landlock"
 
@@ -11,8 +12,13 @@ file_medium = 'perf-results/execution-times-medium.csv'
 stats_simple = 'perf-results/stats-simple.csv'
 stats_medium = 'perf-results/stats-medium.csv'
 
+graph_simple = 'perf-results/graph_simple.png'
+graph_simple_c = 'perf-results/graph_simple_c.png'
+graph_medium = 'perf-results/graph_medium.png'
+graph_medium_c = 'perf-results/graph_medium_c.png'
+
 cols = ['args_parsing','module_init','preopen','landlock','running','args_parsing_c','module_init_c','preopen_c','landlock_c','running_c']
-col_names = ['Argument parsing', 'Module initialization', 'Preopen', 'Landlock enforcement', 'Running WASM binary', 'Argument parsing (cumulative)', 'Module initialization (cumulative)', 'Preopen (cumulative)', 'Landlock enforcement (cumulative)', 'Running WASM binary (cumulative)']
+col_names = ['Argument parsing', 'Module initialization', 'Preopen', 'Landlock enforcement', 'Running WASM binary']
 
 wasm_bins = {
     file_simple: './wasm-bin/hello.wasm',
@@ -28,7 +34,7 @@ for file in [file_simple, file_medium]:
 
 for file in [stats_simple, stats_medium]:
     with open(file, 'w') as f:
-        f.write('type,mean,stddev\n')
+        f.write('type,mean,stddev,meanc,stddevc\n')
 
 #%% Getting all data
 
@@ -52,9 +58,30 @@ def write_stats(file, out):
         for col, col_name in zip(cols, col_names):
             mean = df[col].mean()
             std = df[col].std()
-            f.write(f'{col_name},{mean},{std}\n')
+            meanc = df[f'{col}_c'].mean()
+            stdc = df[f'{col}_c'].std()
+            f.write('{},{:0.2f},{:0.2f},{:0.2f},{:0.2f}\n'.format(col_name,mean,std,meanc,stdc))
 
 write_stats(file_simple, stats_simple)
 write_stats(file_medium, stats_medium)
+
+# %% Graphs
+
+def graphs_for(file, out, out_c):
+    df = pd.read_csv(file)
+    xs = list(reversed(df['type']))
+
+    ys = list(reversed(df['mean']))
+    plt.figure()
+    plt.barh(xs, ys)
+    plt.savefig(out)
+
+    ys = list(reversed(df['meanc']))
+    plt.figure()
+    plt.barh(xs, ys)
+    plt.savefig(out_c)
+
+graphs_for(stats_simple, graph_simple, graph_simple_c)
+graphs_for(stats_medium, graph_medium, graph_medium_c)
 
 # %%
