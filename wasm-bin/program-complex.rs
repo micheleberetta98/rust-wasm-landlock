c0ddb39d22ea3ad6a2efb284419331209aef844c
micheleberetta98 <beretta.m98@gmail.com>
2022-08-05 18:39:21 +0200

    Fix

--- wasm-bin/program-complex.rs
+++ wasm-bin/program-complex.rs
@@ -3,6 +3,6 @@ use std::fs::{read_to_string, write};
 fn main() {
   read_to_string("file1.txt").expect("Could not read file1");
   write("file1.txt", "New file 1").expect("Could not write file1");
-  read_to_string("subdir/file2.txt").expect("Could not read file2");
-  write("subdir/file2.txt", "New file 2").expect("Could not write file2");
+  read_to_string("file2.txt").expect("Could not read file2");
+  write("file2.txt", "New file 2").expect("Could not write file2");
 }
