e062fec2776cb0cc8c1630d37f42f98482701fca
micheleberetta98 <beretta.m98@gmail.com>
2022-08-06 14:56:39 +0200

    Add avg and boxplot for landlock impact on num of permissions

--- perf-landlock-helper.py
+++ perf-landlock-helper.py
@@ -1,4 +1,8 @@
+#%% Setup
+
 import os
+import pandas as pd
+import matplotlib.pyplot as plt
 
 tests = [
     'R,W',
@@ -15,6 +19,8 @@ tests = [
     '*',
 ]
 
+#%% Tests
+
 for test in tests:
     l = len(test.split(','))
     file = f'perf-results/landlock-impact-{l}.csv'
@@ -24,3 +30,28 @@ for test in tests:
 
     for _ in range(100):
         os.system(f'cargo run -r -- ./wasm-bin/program-complex.wasm --mapdir="tmp-dir:." --fs-allow="tmp-dir:{test}"')
+
+#%% Graphs
+
+xs = list(range(2, 14))
+means = []
+all_times = []
+for x in xs:
+    file = f'perf-results/landlock-impact-{x}.csv'
+    df = pd.read_csv(file)
+    all_times.append(df['landlock'])
+    means.append(df['landlock'].mean())
+
+plt.figure()
+plt.plot(xs, means)
+plt.xlabel('Number of active permissions')
+plt.ylabel('Time (ns)')
+plt.savefig('perf-results/landlock-impact.png', facecolor='white')
+
+plt.figure()
+plt.boxplot(all_times, showfliers=False)
+plt.xlabel('Number of active permissions')
+plt.ylabel('Time (ns)')
+plt.savefig('perf-results/landlock-impact-box.png', facecolor='white')
+
+# %%
